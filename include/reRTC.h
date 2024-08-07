d9821b6e8fabb617489574ae9f7abff3ba47c497
kotyara12 <kotyara12@yandex.ru>
2024-08-07 19:26:58 +0300

    Nothing significant

--- include/reRTC.h
+++ include/reRTC.h
@@ -2,7 +2,7 @@
    RU: Служба часов реального времени
    EN: Real time clock service
    --------------------------------------------------------------------------------
-   (с) 2022 Разживин Александр | Razzhivin Alexander
+   (с) 2022-2024 Разживин Александр | Razzhivin Alexander
    kotyara12@yandex.ru | https://kotyara12.ru | tg: @kotyara1971
 */
 
@@ -11,10 +11,16 @@
 
 #include "project_config.h"
 
-#if CONFIG_RTC_INSTALLED
-
 #define DS1307 1
 #define DS3231 2
+
+#if defined(CONFIG_RTC_INSTALLED)
+
+#if CONFIG_RTC_TYPE == DS1307
+   #include "reDS1307.h"
+#elif CONFIG_RTC_TYPE == DS3231
+   #include "reDS3231.h"
+#endif // CONFIG_RTC_TYPE
 
 #ifdef __cplusplus
 extern "C" {
